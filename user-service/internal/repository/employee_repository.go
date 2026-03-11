5304cea10dcfd34807545f4445f2921cd815700d
lukasavic <lukavicsa12@gmail.com>
2026-03-11 03:59:37 +0100

    feat(user): add employee repository with CRUD and filtering

---
+++ user-service/internal/repository/employee_repository.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"github.com/exbanka/user-service/internal/model"
+	"gorm.io/gorm"
+)
+
+type EmployeeRepository struct {
+	db *gorm.DB
+}
+
+func NewEmployeeRepository(db *gorm.DB) *EmployeeRepository {
+	return &EmployeeRepository{db: db}
+}
+
+func (r *EmployeeRepository) Create(emp *model.Employee) error {
+	return r.db.Create(emp).Error
+}
+
+func (r *EmployeeRepository) GetByID(id int64) (*model.Employee, error) {
+	var emp model.Employee
+	err := r.db.First(&emp, id).Error
+	return &emp, err
+}
+
+func (r *EmployeeRepository) GetByEmail(email string) (*model.Employee, error) {
+	var emp model.Employee
+	err := r.db.Where("email = ?", email).First(&emp).Error
+	return &emp, err
+}
+
+func (r *EmployeeRepository) Update(emp *model.Employee) error {
+	return r.db.Save(emp).Error
+}
+
+func (r *EmployeeRepository) SetPassword(userID int64, passwordHash string) error {
+	return r.db.Model(&model.Employee{}).Where("id = ?", userID).
+		Updates(map[string]interface{}{"password_hash": passwordHash, "activated": true}).Error
+}
+
+func (r *EmployeeRepository) List(emailFilter, nameFilter, positionFilter string, page, pageSize int) ([]model.Employee, int64, error) {
+	var employees []model.Employee
+	var total int64
+	q := r.db.Model(&model.Employee{})
+
+	if emailFilter != "" {
+		q = q.Where("email ILIKE ?", "%"+emailFilter+"%")
+	}
+	if nameFilter != "" {
+		q = q.Where("first_name ILIKE ? OR last_name ILIKE ?", "%"+nameFilter+"%", "%"+nameFilter+"%")
+	}
+	if positionFilter != "" {
+		q = q.Where("position ILIKE ?", "%"+positionFilter+"%")
+	}
+
+	q.Count(&total)
+
+	if page < 1 {
+		page = 1
+	}
+	if pageSize < 1 {
+		pageSize = 20
+	}
+	offset := (page - 1) * pageSize
+
+	err := q.Offset(offset).Limit(pageSize).Find(&employees).Error
+	return employees, total, err
+}
